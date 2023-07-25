6467b2ff0f938ebc6476a180ca97e4bf42186ed6
jaremko <w3techplayground@gmail.com>
2023-07-25 15:10:46 +0200

    refactor

--- src/transform_to_editable_worker.cljs
+++ src/transform_to_editable_worker.cljs
@@ -1,36 +1,11 @@
 (ns transform-to-editable-worker
-  (:require [profile]
-            [cljs.reader :as reader]
-            [clojure.string :as str]
-            [cljs.spec.alpha :as s]
-            [camel-snake-kebab.core :as csk]
-            [camel-snake-kebab.extras :as cske]
-            [clojure.walk :as walk]))
+  (:require [worker-utils :as wu]
+            [profile]
+            [cljs.spec.alpha :as s]))
 
-(defn process-data [x]
-   (js/console.log "Received message in worker: " (str x))
-  x)
-
-(defn on-message [event]
-  (when event
-    (let [data (.-data event)
-          edn-data (cljs.core/js->clj data :keywordize-keys true) ;; Convert JavaScript object to ClojureScript data structure with keywordized keys
-          kebab-case-data (cske/transform-keys csk/->kebab-case-keyword edn-data) ;; Convert keys to kebab-case
-          mapped-data (profile/specific-mapping kebab-case-data)] ;; Apply specific mappings
-      (if (:err mapped-data)
-        (.postMessage js/self (cljs.core/clj->js mapped-data))
-        (let [normalized-data (profile/walk-divide mapped-data)
-              valid? (s/valid? ::profile/payload normalized-data)]
-          (if valid?
-            (let [processed-data (process-data normalized-data) ;; Process the data
-                  post-processed-data (profile/reverse-mapping processed-data)] ;; Apply reverse mappings to processed data
-              (if (:err post-processed-data)
-                (.postMessage js/self (cljs.core/clj->js post-processed-data))
-                (let [camelCase-data (cske/transform-keys csk/->camelCaseKeyword post-processed-data) ;; Convert keys back to camelCase
-                      result (cljs.core/clj->js camelCase-data)] ;; Convert the processed data back to a JavaScript object
-                  (.postMessage js/self result))))
-            (let [report (s/explain-data ::profile/payload normalized-data)
-                  result (cljs.core/clj->js report)]
-              (.postMessage js/self result))))))))
-
-(set! (.-onmessage js/self) on-message)
+(wu/set-handler
+  profile/specific-mapping
+  profile/walk-divide
+  identity
+  (partial s/valid? ::profile/payload)
+  profile/reverse-mapping)
--- src/worker_utils.cljs
+++ src/worker_utils.cljs
@@ -1,25 +1,42 @@
 (ns worker-utils
   (:require [profile]
+            [cljs.reader :as reader]
+            [clojure.string :as str]
             [cljs.spec.alpha :as s]
             [camel-snake-kebab.core :as csk]
             [camel-snake-kebab.extras :as cske]))
 
-(defn validate-and-transform
-  "Validates and transforms data. Returns an error report if validation fails."
-  [data process-fn]
-  (let [valid? (s/valid? ::profile/payload data)]
-    (if valid?
-      (cljs.core/clj->js (cske/transform-keys csk/->camelCaseKeyword
-                     (profile/reverse-mapping (process-fn data))))
-      (cljs.core/clj->js (s/explain-data ::profile/payload data)))))
+(defn process-data [x]
+   (js/console.log "Received message in worker: " (str x))
+  x)
 
-(defn handle-message
-  "Handles incoming event messages, applying specific mappings and transformations."
-  [event process-fn]
+(defn transform-and-validate [pre-validate-fn post-validate-fn validate-fn data]
+  (let [pre-validated-data (pre-validate-fn data)
+        valid? (validate-fn pre-validated-data)]
+    (if valid?
+      {:data (post-validate-fn pre-validated-data)}
+      {:err (s/explain-data ::profile/payload pre-validated-data)})))
+
+(defn post-process [reverse-mapping-fn data]
+  (let [post-processed-data (reverse-mapping-fn data)]
+    (if (:err post-processed-data)
+      (cljs.core.clj->js post-processed-data)
+      (cske/transform-keys csk/->camelCaseKeyword post-processed-data))))
+
+(defn handle-event [specific-mapping-fn pre-validate-fn post-validate-fn validate-fn reverse-mapping-fn event]
   (when event
-    (let [data (-> event .-data (cljs.core/js->clj :keywordize-keys true))]
-      (.postMessage js/self
-        (if-let [err (:err (profile/specific-mapping data))]
-          (cljs.core/clj->js err)
-          (validate-and-transform (cske/transform-keys csk/->kebab-case-keyword data)
-                                  process-fn))))))
+    (let [data (.-data event)
+          edn-data (cljs.core/js->clj data :keywordize-keys true)
+          kebab-case-data (cske/transform-keys csk/->kebab-case-keyword edn-data)
+          mapped-data (specific-mapping-fn kebab-case-data)]
+      (if (:err mapped-data)
+        (.postMessage js/self (cljs.core/clj->js mapped-data))
+        (let [{:keys [data err]} (transform-and-validate pre-validate-fn post-validate-fn validate-fn mapped-data)]
+          (if err
+            (.postMessage js/self (cljs.core.clj->js err))
+            (let [processed-data (process-data data)
+                  result (post-process reverse-mapping-fn processed-data)]
+              (.postMessage js/self (cljs.core/clj->js result)))))))))
+
+(defn set-handler [specific-mapping-fn pre-validate-fn post-validate-fn validate-fn reverse-mapping-fn]
+  (set! (.-onmessage js/self) (partial handle-event specific-mapping-fn pre-validate-fn post-validate-fn validate-fn reverse-mapping-fn)))
